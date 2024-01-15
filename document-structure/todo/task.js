c10d0462167c45d069ac6083b74a452d74313197
Mitriy_Bug <140303168+Mitriy-Bug@users.noreply.github.com>
2024-01-15 22:40:16 +0300

    Оптимизация кода обработчика события

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -6,9 +6,10 @@ tasksForm.addEventListener('submit', (objectForm) => {
   objectForm.preventDefault();
   value = taskInput.value.trim();
   if(value){
-    tasksList.insertAdjacentHTML("afterbegin", "<div class='task'><div class='task__title'>"+value+"</div><a href='#' class='task__remove'>&times;</a></div>")
+    tasksList.insertAdjacentHTML("afterbegin", "<div class='task'><div class='task__title'>"+value+"</div><a href='#' class='task__remove'>&times;</a></div>");
+    let task = tasksList.children[0];
+    task.querySelector(".task__remove").addEventListener('click', () => task.remove());
   }
   document.getElementById("task__input").value = '';
-  let task = tasksList.children[0];
-  task.querySelector(".task__remove").addEventListener('click', () => task.remove());
+  
 })
